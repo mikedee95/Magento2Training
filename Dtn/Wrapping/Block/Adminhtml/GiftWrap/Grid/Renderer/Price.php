69e7db003c68a194a19375ada110b052202b86af
mikedee95 <70868084+mikedee95@users.noreply.github.com>
2021-07-07 14:29:17 +0700

    Remove Wrapping module

--- Dtn/Wrapping/Block/Adminhtml/GiftWrap/Grid/Renderer/Price.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Dtn\Wrapping\Block\Adminhtml\GiftWrap\Grid\Renderer;
-
-class Price extends \Magento\Backend\Block\Widget\Grid\Column\Renderer\AbstractRenderer
-{
-    private $_storeManager;
-    
-}
--- Dtn/Wrapping/Controller/GiftWrapInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Dtn\Wrapping\Controller;
-
-use Magento\Framework\App\ActionInterface;
-
-interface GiftWrapInterface extends ActionInterface
-{
-}
